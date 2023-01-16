9cb1c3974ced9d531cf3c985eb22d62bc5eff3b9
Parminder Singh <parmindersingh1086@gmail.com>
2023-01-15 17:20:15 -0800

    Display for grid of game is added and gameBoard, player objects are created

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const gameBoard = (() => {
+    let board = Array(9).fill(null);
+    const domBoard = document.querySelector('#gameBoard')
+    const createBoard = () => {
+        for(let i = 0; i < board.length; i++) {
+            let square = document.createElement('button');
+            square.classList.toggle('squares');
+            square.innerHTML = board[i];
+            domBoard.appendChild(square);
+        }
+    };
+    createBoard();
+    const squares = document.querySelectorAll('.squares');
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('click', () =>{
+            squares[i].innerHTML = player.character;
+        });
+    }
+ })();
 
+ const displayController = (() => {
+ })();
+
+const player = (character) => {
+    return {character};
+}
